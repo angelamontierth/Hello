8c09843e879a08b938f52852e46d96cdbc5f91cd
Angela Montierth <angelamontierth@Angelas-MacBook-Pro.local>
2017-02-03 14:43:00 -0700

    Create new Xcode project.

---
+++ Day 5 _ Hello project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day 5 _ Hello project
+//
+//  Created by Angela Montierth on 2/3/17.
+//  Copyright © 2017 Angela Montierth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
